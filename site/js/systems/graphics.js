65102aad63650a380c1a5570ac62cfd2182006e5
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-01-05 19:05:27 -0500

    added broswerify output script into html

--- site/js/systems/graphics.js
+++ site/js/systems/graphics.js
@@ -12,10 +12,7 @@ GraphicsSystem.prototype.run = function() {
 GraphicsSystem.prototype.tick = function() {
     for (var i=0; i<this.entities.length; i++) {
         var entity = this.entities[i];
-        if (!'graphics' in entity.components) {
-            continue;
-        }
-
+        if ('graphics' in entity.components) 
         entity.components.graphics.draw(this.context);
     }
 };
