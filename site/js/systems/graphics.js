998b525f07da410d9c82a4d4a58fc3e12b2529e9
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-01-08 12:01:29 -0500

    updated tick function

--- site/js/systems/graphics.js
+++ site/js/systems/graphics.js
@@ -27,6 +27,10 @@ GraphicsSystem.prototype.tick = function() {
     //Clear the canvas, top-left to bottom-right.
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.context.save();
+    this.context.translate(this.canvas.width/2, this.canvas.height);
+    this.context.scale(this.canvas.height, -this.canvas.height);
+
     //Rendering of graphics goes here
     for (var i=0; i<this.entities.length; i++) {
         var entity = this.entities[i];
@@ -35,6 +39,8 @@ GraphicsSystem.prototype.tick = function() {
         }
         entity.components.graphics.draw(this.context);
     }
+
+    this.context.restore();
 
     //Continue the graphics rendering loop.
     window.requestAnimationFrame(this.tick.bind(this));
