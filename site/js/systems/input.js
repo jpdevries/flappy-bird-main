8f7b3d2cc7ca812d11b0b7f51361f8af0c973b9a
JP DeVries <johnpaul.devries@gmail.com>
2016-01-21 09:55:02 -0800

    spacebar fix

--- site/js/systems/input.js
+++ site/js/systems/input.js
@@ -1,5 +1,4 @@
 var InputSystem = function(entities) {
-    console.log('InputSystem');
     this.entities = entities;
 
     // Canvas is where we get input from
@@ -7,18 +6,17 @@ var InputSystem = function(entities) {
 };
 
 InputSystem.prototype.run = function() {
-    console.log('run');
     this.canvas.addEventListener('click', this.onClick.bind(this));
-    this.canvas.addEventListener('keydown', this.onkeydown.bind(this));
+    document.body.addEventListener('keydown', this.onkeydown.bind(this));
 };
 
 InputSystem.prototype.onClick = function() {
-	console.log("click detected!");
     var bird = this.entities[0];
     bird.components.physics.velocity.y = 0.4;
 };
 
 InputSystem.prototype.onkeydown = function(e) {
+  console.log('keydown');
 	if (e.keyCode == 32) {
 	console.log("spacebar pressed!");
 	var bird = this.entities[0];
