bee4419154a7cce8b2179df0759731e5e010d39d
JP DeVries <mail@devries.jp>
2016-01-30 19:31:49 -0800

    Wing Flapping

--- site/js/components/graphics/bird.js
+++ site/js/components/graphics/bird.js
@@ -2,11 +2,25 @@ var settings = require("../../settings");
 
 var BirdGraphicsComponent = function(entity) {
     this.entity = entity;
+
+    this.flapIndex = 0;
+
+    this.images = [
+      document.getElementById("bird2"),
+      document.getElementById("bird1"),
+      document.getElementById("bird2"),
+      document.getElementById("bird3")
+    ];
+
+    this.interval = window.setInterval(this.flap.bind(this),240);
+};
+
+BirdGraphicsComponent.prototype.flap = function() {
+  this.flapIndex += 1;
+  if(this.flapIndex >= this.images.length) this.flapIndex = 0;
 };
 
 BirdGraphicsComponent.prototype.draw = function(context) {
-    //console.log("Drawing a bird");
-
     var position = this.entity.components.physics.position;
 
     //Save a snapshot of the current transformation state.
@@ -18,19 +32,9 @@ BirdGraphicsComponent.prototype.draw = function(context) {
     //Start drawing a new path by calling the beginPath function.
     context.beginPath();
 
-    //Use the context.arc function to draw an arc centered 0px from the left of the canvas
-    //and 0px from the top of the canvas, w/ radius of 0.02, and covering an angle between 0
-    //and 2 x pi (is a whole circle).
-    //context.arc(0, 0, 0.02, 0, 2 * Math.PI);
-
-    //Use the context.fill function to fill in the arc with whatever the current
-    //context.fillStyle is (this defaults to black unless changed).
-    //context.fill();
-
-    var image = document.getElementById("bird");
     context.scale(-1, 1);
     context.rotate(Math.PI);
-    context.drawImage(image, 0, 0, settings.birdRadius, settings.birdRadius);
+    context.drawImage(this.images[this.flapIndex], 0, 0, settings.birdRadius, settings.birdRadius);
 
     //Stop drawing.
     context.closePath();
